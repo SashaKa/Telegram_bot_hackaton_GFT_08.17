012265351731b0559782ee4ef8cf8c35eebcb6dc
SashaKa <skrableskru@gmail.com>
2017-08-26 22:41:45 +0300

    refactorrr!

--- bot.js
+++ bot.js
@@ -17,7 +17,8 @@ const state = {}
 bot.onText(/^/, function(msg) {
   const name = msg.from.first_name
   const id = msg.chat.id
-  
+  console.log(state)
+
   // // bot.sendMessage(id, `Привет, как тебя зовут?`).then(function() {
   // //   console.log('state', state)
   // })
@@ -34,14 +35,18 @@ bot.onText(/start/, msg => {
   })
 })
 
-bot.onText(/^В школе/, msg => {
+bot.onText(/^(В школе|В университете|В техникуме|Не учусь)$/, (msg, match) => {
   const name = msg.from.first_name
   const id = msg.chat.id
   state[id] = {}
   Object.assign(state[id], {
-    1: msg.text
+    'Answer 1': msg.text
   })
-  bot.sendMessage(id, `${state[id][1]} - здорово!`).then(() => {
+  const botAnswer =
+    match[0] != 'Не учусь'
+      ? `${state[id]['Answer 1']} - здорово!`
+      : 'И это только начало'
+  bot.sendMessage(id, botAnswer).then(() => {
     bot.sendMessage(id, `Что тебе нравится?`, {
       reply_markup: {
         keyboard: [['Лазеры', 'Роботы'], ['Еда', 'Все это']],
@@ -50,111 +55,13 @@ bot.onText(/^В школе/, msg => {
     })
   })
 })
-bot.onText(/^В университете/, msg => {
+bot.onText(/^(Лазеры|Роботы|Еда|Все это)$/, msg => {
   const name = msg.from.first_name
   const id = msg.chat.id
-  state[id] = {}
   Object.assign(state[id], {
-    2: msg.text
+    'Answer 2': msg.text
   })
-  bot.sendMessage(id, `${state[id][2]} - здорово!`).then(() => {
-    bot.sendMessage(id, `Что тебе нравится?`, {
-      reply_markup: {
-        keyboard: [['Лазеры', 'Роботы'], ['Еда', 'Все это']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-bot.onText(/^В техникуме/, msg => {
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    3: msg.text
-  })
-  bot.sendMessage(id, `${state[id][3]} - интересно!`).then(() => {
-    bot.sendMessage(id, `Что тебе нравится?`, {
-      reply_markup: {
-        keyboard: [['Лазеры', 'Роботы'], ['Еда', 'Все это']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-bot.onText(/^Не учусь/, msg => {
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    4: msg.text
-  })
-  bot.sendMessage(id, `И это только начало!`).then(() => {
-    bot.sendMessage(id, `Что тебе нравится?`, {
-      reply_markup: {
-        keyboard: [['Лазеры', 'Роботы'], ['Еда', 'Все это']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-
-bot.onText(/^Лазеры/, msg => {
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    1: msg.text
-  })
-  bot.sendMessage(id, `${state[id][1]} по тебе!`).then(() => {
-    bot.sendMessage(id, `Насколько ты знаешь физику от 0 до 10?`, {
-      reply_markup: {
-        keyboard: [['До 5', '5-7'], ['7-10', 'Совсем не знаю']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-bot.onText(/^Роботы/, msg => {
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    2: msg.text
-  })
-  bot.sendMessage(id, `${state[id][2]} по тебе!`).then(() => {
-    bot.sendMessage(id, `Насколько ты знаешь физику от 0 до 10?`, {
-      reply_markup: {
-        keyboard: [['До 5', '5-7'], ['7-10', 'Совсем не знаю']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-bot.onText(/^Еда/, msg => {
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    3: msg.text
-  })
-  bot.sendMessage(id, `${state[id][3]} по тебе!`).then(() => {
-    bot.sendMessage(id, `Насколько ты знаешь физику от 0 до 10?`, {
-      reply_markup: {
-        keyboard: [['До 5', '5-7'], ['7-10', 'Совсем не знаю']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-bot.onText(/^Все это/, msg => {
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    4: msg.text
-  })
-  bot.sendMessage(id, `Да ты голова!`).then(() => {
+  bot.sendMessage(id, `${state[id]['Answer 2']} по тебе!`).then(() => {
     bot.sendMessage(id, `Насколько ты знаешь физику от 0 до 10?`, {
       reply_markup: {
         keyboard: [['До 5', '5-7'], ['7-10', 'Совсем не знаю']],
@@ -164,63 +71,24 @@ bot.onText(/^Все это/, msg => {
   })
 })
 
-bot.onText(/^Совсем не знаю/, msg => {
+bot.onText(/^(Совсем не знаю|До 5|5-7|7-10)$/, (msg, match) => {
   const name = msg.from.first_name
   const id = msg.chat.id
-  state[id] = {}
   Object.assign(state[id], {
-    4: msg.text
+    'Answer 3': msg.text
   })
-  bot.sendMessage(id, `попробуем подтянуть!`).then(() => {
-    bot.sendMessage(id, `Выбери удобный тебе формат обучения!`, {
-      reply_markup: {
-        keyboard: [['Смотреть', 'Читать'], ['Слушать', 'На практике']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-bot.onText(/^До 5/, msg => {
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    1: msg.text
-  })
-  bot.sendMessage(id, `${state[id][1]} - есть с чего начать!`).then(() => {
-    bot.sendMessage(id, `Выбери удобный тебе формат обучения!`, {
-      reply_markup: {
-        keyboard: [['Смотреть', 'Читать'], ['Слушать', 'На практике']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-bot.onText(/^5-7/, msg => {
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    2: msg.text
-  })
-  bot.sendMessage(id, `Хорошо! Продолжим?`).then(() => {
-    bot.sendMessage(id, `Выбери удобный тебе формат обучения!`, {
-      reply_markup: {
-        keyboard: [['Смотреть', 'Читать'], ['Слушать', 'На практике']],
-        one_time_keyboard: true
-      }
-    })
-  })
-})
-bot.onText(/^7-10/, msg => {
-  bot.sendSticker(msg.chat.id, 'CAADAgADrAADBCOmDIRBbsLh2lHxAg')
-  const name = msg.from.first_name
-  const id = msg.chat.id
-  state[id] = {}
-  Object.assign(state[id], {
-    3: msg.text
-  })
-  bot.sendMessage(id, `${state[id][3]} - Так держать! Будет интересно!`).then(() => {
+  let botAnswer
+  switch (match[0]) {
+    case 'Совсем не знаю':
+      botAnswer = 'попробуем подтянуть!'
+    case 'До 5':
+      botAnswer = '2!'
+    case '5-7':
+      botAnswer = '3!'
+    case '7-10':
+      botAnswer = '4!'
+  }
+  bot.sendMessage(id, botAnswer).then(() => {
     bot.sendMessage(id, `Выбери удобный тебе формат обучения!`, {
       reply_markup: {
         keyboard: [['Смотреть', 'Читать'], ['Слушать', 'На практике']],
@@ -230,12 +98,11 @@ bot.onText(/^7-10/, msg => {
   })
 })
 
-bot.onText(/^Смотреть/, msg => {
+bot.onText(/^(Смотреть)$/, msg => {
   const name = msg.from.first_name
   const id = msg.chat.id
-  state[id] = {}
   Object.assign(state[id], {
-    3: msg.text
+    'Answer 4': msg.text
   })
   bot
     .sendMessage(id, `${state[id][3]} - Так держать! Будет интересно!`)
@@ -253,11 +120,10 @@ bot.onText(/^sticker/, msg => {
   bot.sendSticker(msg.chat.id, 'CAADAgADrAADBCOmDIRBbsLh2lHxAg')
 })
 bot.onText(/^video/, msg => {
-  bot.InlineQueryResultVideo (msg.chat.id,'' )
+  bot.InlineQueryResultVideo(msg.chat.id, '')
 })
 bot.onText(/^Gif/, msg => {
-  bot.InlineQueryResultGif(msg.chat.id,'')
+  bot.InlineQueryResultGif(msg.chat.id, '')
 })
-
 
 module.exports = bot
